f078367e1357c3227331f543811d55151df3ac41
YuQiao Huang <leah.huang0428@gmail.com>
2022-05-03 11:54:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/twod_s1090341/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.twod_s1090341
+
+import android.graphics.Canvas
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.twod_s1090341.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var secondsLeft:Int = 1000  //倒數
+    lateinit var job: Job
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.txv.text = secondsLeft.toString()
+        binding.btnStart.isEnabled = true
+        binding.btnStop.isEnabled = false
+
+
+        binding.btnStart.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                binding.mysv.Score = 0
+                //binding.txv.text = "開始倒數計時"
+                job = GlobalScope.launch(Dispatchers.Main) {
+                    while(secondsLeft > 0) {
+                        secondsLeft--
+                        binding.txv.text = secondsLeft.toString()
+                        binding.btnStart.isEnabled = false
+                        binding.btnStop.isEnabled = true
+                        delay(25)
+
+                        val canvas: Canvas = binding.mysv.holder.lockCanvas()
+                        binding.mysv.drawSomething(canvas)
+                        binding.mysv.holder.unlockCanvasAndPost(canvas)
+                    }
+                    secondsLeft = 1000
+                    binding.btnStart.isEnabled = true
+                    binding.btnStop.isEnabled = false
+                }
+
+            }
+
+        })
+
+        binding.btnStop.setOnClickListener(object:View.OnClickListener{
+            override fun onClick(v: View?) {
+                //binding.txv.text = "結束倒數計時"
+                job.cancel()
+                binding.btnStart.isEnabled = true
+                binding.btnStop.isEnabled = false
+            }
+
+        })
+    }
+    override fun onPause() {
+        super.onPause()
+        job.cancel()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (binding.btnStart.isEnabled == false){
+            job = GlobalScope.launch(Dispatchers.Main) {
+                while(secondsLeft > 0) {
+                    secondsLeft--
+                    binding.txv.text = secondsLeft.toString()
+                    delay(25)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/twod_s1090341/MySurfaceView.kt
@@ -0,0 +1,130 @@
+package com.example.twod_s1090341
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class MySurfaceView(context: Context?, attrs: AttributeSet?)
+    : SurfaceView(context, attrs),SurfaceHolder.Callback {
+     var surfaceHolder: SurfaceHolder
+     var BG:Bitmap
+     var SuperMan: Bitmap
+    var BGmoveX:Int = 0
+
+    var xPos:Int = 0
+    var yPos:Int = 0
+    var deltaX:Int = 5
+    var deltaY:Int = 5
+    lateinit var Player:Bitmap
+    var PlayerX:Float = 0f
+    var PlayerY:Float = 0f
+    var Score:Int = 0  //成績
+    var Shooting:Int = 0  //消失時間
+    var bird1:Bird
+    var bird2:Bird
+    var bird3:Bird
+
+    init {
+        surfaceHolder = getHolder()
+        BG = BitmapFactory.decodeResource(getResources(), R.drawable.back)
+        SuperMan = BitmapFactory.decodeResource(getResources(), R.drawable.superman)
+        surfaceHolder.addCallback(this)
+
+        Player = BitmapFactory.decodeResource(getResources(), R.drawable.player)
+        bird1 = Bird(context!!)
+
+        bird2 = Bird(context!!)
+        bird2.BirdY = 100
+        bird2.BirdX  -= 50
+
+        bird3 = Bird(context!!)
+        bird3.BirdY = 550
+        bird3.BirdY -= 50
+    }
+
+    override fun surfaceCreated(p0: SurfaceHolder) {
+        var canvas: Canvas = surfaceHolder.lockCanvas()
+        drawSomething(canvas)
+        surfaceHolder.unlockCanvasAndPost(canvas)
+
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+    }
+
+    fun drawSomething(canvas:Canvas) {
+        //canvas.drawBitmap(BG, 0f, 0f, null)
+        // 背景捲動
+        BGmoveX--
+        var BGnewX: Int = BG.width + BGmoveX
+        // 如果已捲動 整張圖，則  重新開始
+        if (BGnewX <= 0) {
+            BGmoveX = 0
+
+            // only need one drawcanvas.drawBitmap(BG, BGmoveX.toFloat(), 0f, null)}
+        } else {
+            // need to draw original and wrap
+            canvas.drawBitmap(BG, BGmoveX.toFloat(), 0f, null)
+            canvas.drawBitmap(BG, BGnewX.toFloat(), 0f, null)
+        }
+
+        //超人反彈
+        var SrcRect:Rect = Rect(0, 0, SuperMan.width, SuperMan.height) //裁切
+        var w:Int = SuperMan.width / 6
+        var h:Int = SuperMan.height / 6
+
+        xPos += deltaX
+        yPos += deltaY
+        if (xPos >= getWidth()-w || xPos<=0){
+            deltaX*=-1
+        }
+        if (yPos >= getHeight()-h || yPos<=0){
+            deltaY*=-1
+        }
+        //var DestRect:Rect = Rect(0, 0, w, h)
+        var DestRect:Rect = Rect(xPos, yPos, w + xPos, h + yPos)
+        if (Shooting>0){
+            Shooting--
+        }else
+        {
+            canvas.drawBitmap(SuperMan, SrcRect, DestRect, null)
+        }
+        canvas.drawBitmap(Player, PlayerX, PlayerY, null)
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        paint.color = Color.BLUE
+        paint.textSize = 50f
+        canvas.drawText("Score:"+Score.toString(), 50f,50f, paint)
+
+        bird1.draw(canvas)
+        bird2.draw(canvas)
+        bird3.draw(canvas)
+    }
+
+
+
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        PlayerX = event!!.x
+        PlayerY = event!!.y
+        var w:Int = SuperMan.width / 6
+        var h:Int = SuperMan.height / 6
+        if ((PlayerX>=xPos) && (PlayerX<=xPos+w) && (PlayerY>=yPos) && (PlayerY<=yPos+h)){
+            Score++
+            Shooting = 10
+        }
+        PlayerX -= Player.width / 2
+        PlayerY -= Player.height / 2
+        return false
+    }
+}
+
+
+
+
+
